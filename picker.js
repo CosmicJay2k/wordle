f0991c5f63a3e0055eccd9497cfc12c41f663974
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-03-12 18:10:31 +0100

    Add function for picking word from list

---
+++ picker.js
@@ -0,0 +1,31 @@
+export function wordPicker(list, nr, uniqueOrNot) {
+  // Filter based on number
+  let tempArray = list.filter((word) => word.length === nr);
+
+  // Filter based on uniqueness
+  let excludedArray = list.filter((word) => {
+    const wordArr = Array.from(word);
+    for (let [index, letter] of wordArr.entries()) {
+      if (uniqueOrNot && wordArr.indexOf(letter) < index) {
+        return true;
+      }
+    }
+  });
+
+  // Remove excluded words
+  tempArray = tempArray.filter((word) => !excludedArray.includes(word));
+
+  // Choose random word from filtered list
+  let chosenWord = tempArray[Math.floor(Math.random() * tempArray.length)];
+
+  // If no word meet criteria
+  if (chosenWord === undefined) {
+    chosenWord = "Det finns tyvärr inget ord som passar!";
+  }
+  console.log(chosenWord);
+  return chosenWord;
+}
+
+/* DEV
+const listOfWords = ["älska", "hatar", "träsk", "allvarligt", "vadar"];
+wordPicker(listOfWords, 5, true); */
